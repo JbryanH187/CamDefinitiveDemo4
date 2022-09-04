bf166e80b8a95c9781a3227ac73c75ecfec77508
braya <braya@MASTERRACE>
2022-09-04 10:37:13 -0600

    Agregar archivos de proyecto.

---
+++ CamDefinitiveDemo4/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CamDefinitiveDemo4.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CamDefinitiveDemo4
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new CameraViewPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
